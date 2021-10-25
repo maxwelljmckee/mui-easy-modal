0e4422607a2d07dd1ee54e39e35b102033c7fe5b
Maxwell McKee <maxwelljmckee@gmail.com>
2021-10-24 22:49:15 -0600

    refactor

--- components/useModal.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const useModal = () => {
-  const { open, showModal, hideModal, deferShowModal, deferHideModal } =
-    React.useContext(ModalContext);
-  return { open, showModal, hideModal, deferShowModal, deferHideModal };
-};
